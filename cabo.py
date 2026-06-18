6d102da50a8d5a1c31047b26394ecb0963a7c7d7
GeorgeAndreopoulos <george.andreo@outlook.com>
2026-06-18 19:49:09 +0300

    first commit

---
+++ cabo.py
@@ -0,0 +1,28 @@
+deck = []
+suits = ['S','H','D','C'] #Spades, Hearts, Diamonds, Clubs
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+for s in suits:
+    for n in numbers:
+        deck.append(s + str(n))
+
+#jokers:
+deck.append("A0")
+deck.append("B0")
+
+values = {
+    0: -2,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 0,
+    11: 11,
+    12: 12,
+    13: 13
+}
+
